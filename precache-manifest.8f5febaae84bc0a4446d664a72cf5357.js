c2a2a3e02dee7cf3cd29f0a0e591e121c6dc2505
Mayra <mayra@iMac-de-Mayra.local>
2021-12-13 08:09:27 -0800

    Updates

---
+++ precache-manifest.8f5febaae84bc0a4446d664a72cf5357.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d494e77084afb8f512c2439aa31e495b",
+    "url": "/bank-2/index.html"
+  },
+  {
+    "revision": "e24e86cc3f00a4a85596",
+    "url": "/bank-2/static/css/2.b3d21b21.chunk.css"
+  },
+  {
+    "revision": "98809728a3258c6c45f3",
+    "url": "/bank-2/static/css/main.862d2e67.chunk.css"
+  },
+  {
+    "revision": "e24e86cc3f00a4a85596",
+    "url": "/bank-2/static/js/2.8b7fc07a.chunk.js"
+  },
+  {
+    "revision": "9143a7c0cbfa518ce39da65db7eaf56a",
+    "url": "/bank-2/static/js/2.8b7fc07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98809728a3258c6c45f3",
+    "url": "/bank-2/static/js/main.5bb9672f.chunk.js"
+  },
+  {
+    "revision": "656c07b2c8391481f492",
+    "url": "/bank-2/static/js/runtime-main.22a7bb19.js"
+  },
+  {
+    "revision": "a142bddbf29ee5867520bdeceb165af6",
+    "url": "/bank-2/static/media/cbanktokens.a142bddb.jpg"
+  },
+  {
+    "revision": "adca41d3777e83416e7ad919fa0da8ac",
+    "url": "/bank-2/static/media/dbank.adca41d3.png"
+  },
+  {
+    "revision": "1e0bf434ddab05d4f04d07c9a14602a3",
+    "url": "/bank-2/static/media/etherlogo.1e0bf434.jpg"
+  }
+]);
